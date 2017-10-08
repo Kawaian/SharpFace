43c38c16b176263f2105985ea33770bea0eeb9b0
AinL <gmlwns5176@gmail.com>
2017-10-09 00:39:01 +0900

    wt

---
+++ SharpFace.Tests.Windows/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpFace.Tests.Windows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LandmarkDetector d = new LandmarkDetector();
+        }
+    }
+}
