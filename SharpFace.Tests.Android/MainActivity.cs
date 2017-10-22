e34bb0c9ca6b1384ee35da71d2e5f473bd6c44d8
MattMatt2000 <mconstanshu@gmail.com>
2017-10-22 07:38:48 +0200

    Added test project for android

---
+++ SharpFace.Tests.Android/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace SharpFace.Tests.Android
+{
+    [Activity(Label = "SharpFace.Tests.Android", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Run LandmarkDetector tests
+            SharpFace.Android.Native.Init();
+
+            TestBase t =
+                //new LandmarkTestVid();
+                new LandmarkWrapperTest();
+            var ret = t.Run();
+
+            // Console.Write($"======== Test Finished {ret} =======");
+            // Console.Read();
+        }
+    }
+}
+
